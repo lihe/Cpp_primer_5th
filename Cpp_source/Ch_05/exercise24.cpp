2c581cc8f6cf8d29add5e79eac21e84b5754f156
lihe <lihellowd@outlook.com>
2021-02-03 19:13:29 +0800

    Cpp_source

---
+++ Cpp_source/Ch_05/exercise24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    cout << "请依次输入被除数和除数：" << endl;
+    int ival1, ival2;
+    cin >> ival1 >> ival2;
+    if (ival2 == 0){
+        throw runtime_error("除数不能为0");
+    }
+    cout << "两数相除的结果是：" << ival1 / ival2 << endl;
+    return 0;
+}
