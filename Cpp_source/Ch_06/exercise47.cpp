ae5d3a0ef697ca46c396af70cf8ac7c55bc84bec
lihe <lihellowd@outlook.com>
2021-02-05 14:39:54 +0800

    Cpp_source

--- Cpp_source/Ch_06/exercise47.cpp
+++ Cpp_source/Ch_06/exercise47.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 void print(vector<int> vInt, unsigned index){
     unsigned sz = vInt.size();
+
     // 在此处输出调试信息
     #ifdef NDEBUG
     cout << "vector的对象的大小是：" << sz << endl;
     #endif
+    
     if (!vInt.empty() && index < sz){
         cout << vInt[index] << endl;
         print(vInt, index + 1);
