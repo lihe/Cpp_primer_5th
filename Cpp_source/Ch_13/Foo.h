8c108e58889775b28f17db747273fb7bf25ebaf3
lihe <lihellowd@outlook.com>
2021-02-28 14:42:00 +0800

    Cpp_source

--- Cpp_source/Ch_13/Foo.h
+++ Cpp_source/Ch_13/Foo.h
@@ -20,15 +20,17 @@ private:
     vector<int> data;
 };
 
+// 本对象为右值，所以可以原址排序
 Foo Foo::sorted() &&{
     cout << "右值引用版本" << endl;
     sort(data.begin(), data.end());
     return *this;
 }
 
+// 本对象是const或是一个左值，哪种情况我们都不能对其原址排序
 Foo Foo::sorted() const &{
     cout << "左值引用版本" << endl;
-    Foo ret(*this);
+    Foo ret(*this);               // 拷贝一个副本
     //return ret.sorted();
     return Foo(*this).sorted();
 }
